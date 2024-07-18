5ad7783b67da17a8bb9bc88100f348482fa0cf45
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-07-18 14:47:21 -0600

    Update build files

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="brassy",
     version="0.0.0",
     author="BSL5",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     entry_points={
-        "console_scripts": ["brassy=src.brassy.brassy:run_from_CLI"],
+        "console_scripts": ["brassy=brassy.brassy:run_from_CLI"],
     },
-    install_requires=["argparse","rich"],
+    install_requires=["argparse", "rich"],
 )
