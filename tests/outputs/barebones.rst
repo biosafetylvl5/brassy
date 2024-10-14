b208ecb150803bd8e79754050255da762e34c998
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-10-14 11:32:36 -0600

    Add integration tests, fix bug with release dates being required, add flexible dates to brassy release note date fields

---
+++ tests/outputs/barebones.rst
@@ -0,0 +1,16 @@
+Version [UNKNOWN] (2024-10-14)
+******************************
+
+ * *Bug fix*: Test bug fix.
+
+Bug fix
+=======
+
+Test bug fix.
+-------------
+
+This fixes all computer bugs ever written.
+
+::
+
+    moved: linux/src
---
+++ tests/test_integ.py
@@ -0,0 +1,52 @@
+import subprocess
+import tempfile
+import shutil
+import os
+
+import pytest
+
+
+def run_cli_command_return_true_if_command_returns_zero(command):
+    result = subprocess.run(command, capture_output=True, text=True)
+    assert (
+        result.returncode == 0
+    ), f"Command failed with return code {result.returncode}"
+
+
+@pytest.mark.integtest
+def test_help(monkeypatch):
+    run_cli_command_return_true_if_command_returns_zero(["brassy", "-h"])
+
+
+@pytest.mark.integtest
+@pytest.mark.parametrize(
+    "input_file", ["barebones", "mostly-featured", "fully-featured"]
+)
+def test_build_on_test_files(input_file):
+    with tempfile.TemporaryDirectory() as output_file_dir:
+        output_file = os.path.join(output_file_dir, input_file + ".rst")
+        print(output_file)
+        run_cli_command_return_true_if_command_returns_zero(
+            [
+                "brassy",
+                f"./tests/inputs/{input_file}.yaml",
+                "--output-file",
+                output_file,
+            ]
+        )
+        assert [row for row in open(output_file)] == [
+            row for row in open(f"./tests/outputs/{input_file}.rst")
+        ]
+
+
+def test_pruning():
+    with tempfile.TemporaryDirectory() as output_file_dir:
+        to_prune = os.path.join(output_file_dir, "to-prune.yaml")
+        shutil.copyfile(f"./tests/inputs/to-prune.yaml", to_prune)
+
+        run_cli_command_return_true_if_command_returns_zero(
+            ["brassy", "--prune", to_prune]
+        )
+        assert [row for row in open(to_prune)] == [
+            row for row in open("./tests/outputs/pruned.yaml")
+        ]
