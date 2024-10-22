c085e09f1a729701bb9c8a9c4e02d595872d5920
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-10-22 16:26:36 -0600

    Make 3.7 compatible

--- src/brassy/__init__.py
+++ src/brassy/__init__.py
@@ -1,3 +1,16 @@
-import importlib.metadata
+import logging
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+try:
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except ModuleNotFoundError:
+    try:
+        import importlib_metadata
+
+        __version__ = importlib_metadata.version(__package__ or __name__)
+    except ModuleNotFoundError:
+        logging.debug(
+            "Could not set __version__ because importlib.metadata is not available."
+            + "If running python 3.7, installing importlib-metadata will fix this issue"
+        )
