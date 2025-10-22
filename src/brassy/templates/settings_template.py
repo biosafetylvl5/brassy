6a9b4b26be2f7ab81c517679ec1e5ec904102bbf
Gwyn Uttmark <biosafetylevel5@gmail.com>
2025-10-22 21:58:32 +0000

    use typing List for 3.8 compatibility

--- src/brassy/templates/settings_template.py
+++ src/brassy/templates/settings_template.py
@@ -1,10 +1,11 @@
 import pathlib
 
 from pydantic import BaseModel, Field
+from typing import List
 
 
 class ReleaseTemplate(BaseModel):
-    release_template: list[dict[str, list[str]]] | None = Field(
+    release_template: List[dict[str, List[str]]] | None = Field(
         default=None, alias="release-template",
     )
 
@@ -77,7 +78,7 @@ DefaultTemplate = ReleaseTemplate(
 class SettingsTemplate(BaseModel):
     use_color: bool = True
     default_yaml_path: pathlib.Path | None = None
-    change_categories: list[str] = [
+    change_categories: List[str] = [
         "bug fix",
         "enhancement",
         "deprecation",
@@ -91,7 +92,7 @@ class SettingsTemplate(BaseModel):
     fail_on_empty_dir: bool = True
     description_populates_with_pipe: bool = False
 
-    valid_fields: list[str] = ["title", "description", "files", "related-issue"]
-    valid_changes: list[str] = ["deleted", "moved", "added", "modified"]
+    valid_fields: List[str] = ["title", "description", "files", "related-issue"]
+    valid_changes: List[str] = ["deleted", "moved", "added", "modified"]
     enable_experimental_features: bool = False
     templates: ReleaseTemplate | None = DefaultTemplate
